b9de97770ff1d093b2d816a53d61a2fc6ccc56f7
Ibrahim Tahiri <54563@etu.he2b.be>
2023-12-09 23:48:10 +0100

    adding

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Liste des Étudiantss</title>
+    <title>Liste des Étudiants</title>
 </head>
 <body>
     <menu>
